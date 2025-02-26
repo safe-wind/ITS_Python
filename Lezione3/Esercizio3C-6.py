81b6009530ab5d65c5c6c254cf5b3bdbbfc68214
safe-wind <gdl.marius0@gmail.com>
2025-02-26 12:22:48 +0100

    es3C-6

--- Lezione3/Esercizio3C-6.py
+++ Lezione3/Esercizio3C-6.py
@@ -10,55 +10,50 @@ dizionario: dict = {}
 
 match animal:
 
-    case animal if animal in ["cane", "gatto", "cavallo", "elefante", "leone"]:
+    case animal if animal in ["cane", "gatto", "cavallo", "elefante", "leone", "balena", "delfino"]:
 
         print(f"{animal} appartiene alla categoria dei mammiferi")
         animal_type = "mammiferi"
-        dizionario_mammiferi = dizionario.update({"animale" : animal, "habitat" : habitat, "tipo" : animal_type})
+        dizionario = {"animale" : animal, "habitat" : habitat, "tipo" : animal_type}
 
-        match dizionario_mammiferi:
+        match dizionario:
 
-            case {"animale": animal, "habitat": "acqua", "tipo": "mammiferi"}:
+            case animal if ["balena", "delfino"] and habitat=="acqua":
                 print(f"L'animale {animal} è uno dei mammiferi che può vivere in acqua!")
-            case {"animale": animal, "habitat": "terra", "tipo": "mammiferi"}:
+            case animal if ["cane", "gatto", "cavallo", "elefante", "leone"] and habitat=="terra":
                 print(f"L'animale {animal} è uno dei mammiferi che può vivere sulla terra !")
             case _:
                 print("Errore")
-
-            
                 
 
-    
     case animal if animal in ["serpente", "lucertola", "tartaruga", "coccodrillo"]:
 
         print(f"{animal} appartiene alla categoria dei rettili")
         animal_type = "rettili"
-        dizionario_rettili = dizionario.update({"animale" : animal, "habitat" : habitat, "tipo" : animal_type})
+        dizionario = {"animale" : animal, "habitat" : habitat, "tipo" : animal_type}
 
-        match dizionario_rettili:
+        match dizionario:
 
-            case {"animale": animal, "habitat": "acqua", "tipo": "rettili"}:
-                print(f"L'animale {animal} è uno dei rettili che può vivere in acqua!")
-            case {"animale": animal, "habitat": "terra", "tipo": "rettili"}:
-                print(f"L'animale {animal} è uno dei rettili che può vivere sulla terra !")
+            case animal if ["coccodrillo", "tartaruga", "serpente"] and habitat=="acqua":
+                print(f"L'animale {animal} è uno dei mammiferi che può vivere in acqua!")
+            case animal if ["serpente", "lucertola", "tartaruga"] and habitat=="terra":
+                print(f"L'animale {animal} è uno dei mammiferi che può vivere sulla terra !")
             case _:
                 print("Errore")
 
 
-    case animal if animal in ["aquila", "pappagallo", "gufo", "falco"]:
+    case animal if animal in ["aquila", "pappagallo", "gufo", "falco", "cigno", "anatra", "gallina", "tacchino"]:
 
         print(f"{animal} appartiene alla categoria degli uccelli")
         animal_type = "uccelli"
-        dizionario_uccelli = dizionario.update({"animale" : animal, "habitat" : habitat, "tipo" : animal_type})
+        dizionario = dizionario.update({"animale" : animal, "habitat" : habitat, "tipo" : animal_type})
 
-        match dizionario_uccelli:
+        match dizionario:
 
-            case {"animale": animal, "habitat": "aria", "tipo": "uccelli"}:
-                print(f"L'animale {animal} è uno degli uccelli che può vivere in aria!")
-            case {"animale": animal, "habitat": "terra", "tipo": "uccelli"}:
-                print(f"L'animale {animal} è uno degli uccelli che può vivere sulla terra !")
-            case {"animale": animal, "habitat":"acqua", "tipo":"uccelli"}:
-                print(f"L'animale {animal} è uno degli uccelli che può vivere in acqua !")
+            case animal if ["aquila", "pappagallo", "gufo", "falco", "cigno", "anatra"] and habitat=="aria":
+                print(f"L'animale {animal} è uno dei mammiferi che può vivere in acqua!")
+            case animal if ["gallina", "tacchino"] and habitat=="terra":
+                print(f"L'animale {animal} è uno dei mammiferi che può vivere sulla terra !")
             case _:
                 print("Errore")
     
@@ -66,14 +61,15 @@ match animal:
 
         print(f"{animal} appartiene alla categoria dei pesci")
         animal_type = "pesci"
-        dizionario_pesci = dizionario.update({"animale" : animal, "habitat" : habitat, "tipo" : animal_type})
+        dizionario = {"animale" : animal, "habitat" : habitat, "tipo" : animal_type}
 
-        match dizionario_pesci:
+        match dizionario:
 
-            case {"animale": animal, "habitat":"acqua", "tipo":"pesci"}:
-                print(f"L'animale {animal} è uno degli uccelli che può vivere in acqua !")
+            case animal if ["squalo", "trota", "salmone", "carpa"] and habitat=="acqua":
+                print(f"L'animale {animal} è uno dei mammiferi che può vivere in acqua!")
             case _:
                 print("Errore")
+    
     
     case _:
 
