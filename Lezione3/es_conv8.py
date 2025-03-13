2c66869e219c9002273f042010ae2d54d801d268
safe-wind <gdl.marius0@gmail.com>
2025-03-13 12:52:15 +0100

    es conv flowcharts

--- Lezione3/es_conv8.py
+++ Lezione3/es_conv8.py
@@ -9,15 +9,17 @@ a:int = abs(int(input("Inserisci il numero A: ")))
 b:int = abs(int(input("Inserisci il numero B: ")))
 
 if a < b:
+
     if a > 0 and b > 0:
+
         if a%1 == 0 and b%1 == 0:
+
             somma=0
             i=a
             while i <= b:
 
-                for iter in range(a, b +1):
-                    somma += iter
-                    i+=1
+                somma += i
+                i+=1
         else:
             print("Errore: A e B devono essere interi")
     else:
