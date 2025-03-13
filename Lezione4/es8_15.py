c864bc9f0d90b64c92276c75b562f43186fbb584
safe-wind <gdl.marius0@gmail.com>
2025-03-13 13:31:36 +0100

    es funzioni

---
+++ Lezione4/es8_15.py
@@ -0,0 +1,8 @@
+# Put the functions for the example printing_models.py in a 
+# separate file called printing_functions.py.
+#  Write an import statement at the top of printing_models.py,
+#  and modify the file to use the imported functions.
+
+from es8_16 import prinMod
+
+print(prinMod("999"))
---
+++ Lezione4/es8_16.py
@@ -0,0 +1,11 @@
+
+#funzione es 8_15
+
+def prinMod(par1:str) -> str:
+
+    ninex3 = par1*3
+
+    return ninex3
+
+
+    
