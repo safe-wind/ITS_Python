8a55a088147dc35b397696bb49fc8461edabc047
safe-wind <gdl.marius0@gmail.com>
2025-03-27 16:54:53 +0100

    es fn ricorsive

---
+++ Lezione13/esercizio3.py
@@ -0,0 +1,20 @@
+# Il fattoriale di un intero non negativo n, scritto n!, è il prodotto
+
+# n * (n-1) * (n-2) * ... * 1
+
+# con 1! uguale a 1 e 0! definito come 1.
+# Si scriva una funzione ricorsiva recursiveFactorial che dato un numero n calcoli n!.
+
+
+def recursiveFactorial(n:int) -> int:
+    factorial = 1
+    
+    if n <= 0:
+        return factorial
+    
+    else:
+        
+        return n * recursiveFactorial(n-1)
+    
+
+#print(recursiveFactorial(3))
