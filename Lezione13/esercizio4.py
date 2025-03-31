edfa4a9083096a7cf7d81b27d17941a00f7b2f86
safe-wind <gdl.marius0@gmail.com>
2025-03-31 14:59:55 +0200

    es fn ricorsive

--- Lezione13/esercizio4.py
+++ Lezione13/esercizio4.py
@@ -9,7 +9,7 @@ def recursiveCounter(n:int) -> int:
                                                    # |==> else return *(1)+recCount(n/10) (*)1 il caso dove n = 100
                                                    # si richiama la funzione ^^^^^^^^      |n = 1 cifra che
                                                    # step2. si esegue                      |aggiunge 1      n = 100
-                                                   # (1+)100/10 = (1+)10 -> no return 1+ at step2              n=100/10 -> n=10
+                                                   # (1+)100/10 = (1+)10 -> no return 1+ at step2           n=100/10 -> n=10
                                                    # step3. (1+)10/10 = 1+1 -> return 2 at step3            n=1
                                                    # step4. return 3
     
