b43a488f6c2d5c2beb495cbdc0a2d31107c52060
safe-wind <gdl.marius0@gmail.com>
2025-04-02 12:35:38 +0200

    es funzioni

--- Lezione5/es_lepre_tartaruga.py
+++ Lezione5/es_lepre_tartaruga.py
@@ -1,41 +1,69 @@
-# percorso = 70 quadrati
-# posizione = 1 quadrato 
-# traguardo = 70
-# if gareggiatore >= 70 ---> WIN!
 
-# orologio = 0
+
+
 
 from random import randint
-
+table = ["_"]*70
 
 def percorso(mossaT:int, mossaL:int) -> list[str]:
     if mossaT == mossaL:
         print("OUCH!")
-    percorso = ["_"]*70
-    return percorso
+    
 
 def tartaruga(posizione:int = 1):
+    tar = "T"
+    count_t = 0
+    
+    mossaT = randint(1, 10)
+    
+    if mossaT >= 1 and mossaT<=5:
+        posizione += 3
+    elif mossaT >= 6 and mossaT<=7:
+        posizione-=6
+        if posizione<1:
+            posizione=1
+    else:
+        posizione+=1
 
-
-    while posizione < 70:
-        mossaT = randint(0, 10)
-        pass
+    pass
                 
 
 
 def lepre(posizione:int = 1):
+    lep = "L"
+    count_l = 0
 
     while posizione < 70:
         mossaL = randint(1,10)
-        #match mossaL:
-        pass
+        if mossaL >= 1 and mossaL<= 2:#riposo
+            posizione+=0
+        elif mossaL >=3 and mossaL<=4:#grande balzo
+            posizione+=9
+        elif mossaL==5:#grande scivolata
+            posizione-=12
+            if posizione < 1:
+                posizione = 1
+        elif mossaL >= 6 and mossaL<=8:#piccolo balzo
+            posizione += 1
+        else: #piccola scivolata
+            posizione-=2
+            if posizione < 1:
+                posizione=1
+    
+        if posizione == 70:
+            print("GAME WIN")
+        count_l += 1
+
+        
 
 
-print(percorso(mossaL=1, mossaT=1))
+print(lepre())
 
 
 #gioco
 
 while True:
     print("BANG! AND THEY'RE OFF!!")
+    tartaruga()
+    lepre()
     break
