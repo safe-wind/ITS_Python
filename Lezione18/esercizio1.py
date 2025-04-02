522b5984183d686e2f6d9cb0ffdf610025b12402
safe-wind <gdl.marius0@gmail.com>
2025-04-02 19:15:50 +0200

    es eccezioni

---
+++ Lezione18/esercizio1.py
@@ -0,0 +1,2 @@
+# Safe Square Root: Write a function safe_sqrt(number) that calculates the square root of a 
+# number using math.sqrt(). Handle ValueError if the input is negative by returning an informative message.
