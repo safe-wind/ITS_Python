b48a95b35f2321120f1c55f5f6f3d9f6f5aa36d3
safe-wind <gdl.marius0@gmail.com>
2025-04-14 20:01:49 +0200

    regex

--- Lezione18/esercizio1.py
+++ Lezione18/esercizio1.py
@@ -1,2 +1,18 @@
 # Safe Square Root: Write a function safe_sqrt(number) that calculates the square root of a 
 # number using math.sqrt(). Handle ValueError if the input is negative by returning an informative message.
+from math import sqrt
+
+def safe_sqrt(n:int) -> None:
+
+    try:
+        print(sqrt(n))
+    except ValueError:
+        print("ValueError: the number must be positive to calculate the square root.")
+
+    
+
+safe_sqrt(4)
+
+safe_sqrt(25)
+    
+
---
+++ Lezione18/esercizio2.py
@@ -0,0 +1,22 @@
+
+# Password Validation:  Write a function validate_password(password) that checks whether a password meets the following 
+# criteria: Minimum length of 20 characters, At least three uppercase letters, At least four special characters 
+# (non-alphanumeric). If the password is valid, the function should return True. If the password is invalid, 
+# the function should raise a built-in exception (e.g., ValueError) with a message indicating which criteria were not met.
+
+def validate_password(password:str):
+
+    try:
+        for i in password:
+            if i == 
+
+
+
+
+    
+    
+    
+
+
+
+validate_password("s!234EEEPo!!!ixjwbouwobcownpcn")
