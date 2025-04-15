483725853570e330a3f691020e4305623620a79a
safe-wind <gdl.marius0@gmail.com>
2025-04-15 17:19:45 +0200

    docker

---
+++ Lezione23/es3_slide/app.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+def main():
+    matrix = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ])
+
+    print(matrix)
+
+    transposed_matrix = matrix.T
+
+    print(transposed_matrix)
+
+    if __name__=="__main__":
+        main()
