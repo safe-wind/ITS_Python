1bfd78c3e6e3beb295e8c0a11757e9684355018f
safe-wind <gdl.marius0@gmail.com>
2025-05-08 11:32:32 +0200

    file managment

--- Lezione15/prova0.py
+++ Lezione15/prova0.py
@@ -4,6 +4,5 @@
 # print(file) 
 # file.close()
 
-with open("exemple.txt") as file:
-    pass
-    # dopo aver aperto il file WITH lo chiude automaticamente
+with open("exemple.txt", "r+") as file:
+    print(file.read())  # dopo aver aperto il file WITH lo chiude automaticamente
