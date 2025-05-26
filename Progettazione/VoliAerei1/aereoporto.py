11a9a38c4c4c286926ab0ec6263ca77290506c92
safe-wind <gdl.marius0@gmail.com>
2025-05-26 14:49:53 +0200

    aereoporto

--- Progettazione/VoliAerei1/aereoporto.py
+++ Progettazione/VoliAerei1/aereoporto.py
@@ -3,4 +3,10 @@ class Aereoporto:
 
     def __init__(self, cod:str, nome:str):
         self.cod = cod
+        self.setNome(nome)
+
+    def setNome(self, nome:str) -> None:
         self.nome = nome
+    
+    def getNome(self) -> str:
+        return self.nome
