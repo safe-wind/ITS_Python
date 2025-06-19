32d5093fe9850df5cf86f360225ca6ce82557618
safe-wind <gdl.marius0@gmail.com>
2025-06-19 14:49:57 +0200

    alieno class

--- Esercizi_rp/rp3/ReP3_M_Gidilica/alieno.py
+++ Esercizi_rp/rp3/ReP3_M_Gidilica/alieno.py
@@ -3,37 +3,34 @@ import random
 
 class Alieno(Creatura):
 
-    _matricola:int #int > 0
-    
     def __init_subclass__(cls):
         return super().__init_subclass__()
 
     def __init__(self):
-        
-        self._set_matricola()
+        self._matricola=""
+        self._munizioni=[]
+        self.nome = f"Robot-{self._set_matricola()}"
         self._set_munizioni()
 
+    def get_nome(self):
+        return self.nome
+    
     def _set_matricola(self) -> None:
         self._matricola = random.randint(10000,90000)
+        return self._matricola
+
     
     def _get_matricola(self) -> int:
-        return self._matricola
-    
-    def _sequenza_muniz(self, cont_sequenza:int=0, sequenza:int=0)->None:
-        
-        result =cont_sequenza+sequenza
-        cont_sequenza+=1
-        sequenza+=2
-        
-        return result
-        
+        return self._matricola 
     
     def _set_munizioni(self):
 
-        _munizioni:list[int] = list()
+        self._munizioni= [x * x for x in range(0,15)]
+        return self._munizioni
+    
+    def __str__(self):
+        return f"Alieno: {self.get_nome()}"
         
-            
-        return _munizioni
 
 
 
@@ -41,5 +38,7 @@ if __name__=="__main__":
 
     creatura:Creatura =Creatura("Creatur")
     alieno1:Alieno = Alieno()
-    print(alieno1._set_munizioni())
+    alieno1._set_matricola()
     print(alieno1.get_nome())
+    print(alieno1)
+    
