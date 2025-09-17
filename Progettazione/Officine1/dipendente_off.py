e37d66873003ae6195f50c088a86760646df6e5b
safe-wind <gdl.marius0@gmail.com>
2025-09-17 17:17:58 +0200

    reloaded

--- Progettazione/Officine1/dipendente_off.py
+++
@@ -1,9 +0,0 @@
-from persona_officine import Persona
-from indirizzo import Indirizzo
-from telefono import Telefono
-from codfisc import CodiceFiscale
-
-class Dipendente(Persona):
-     
-    def __init__(self, name, last, cf:CodiceFiscale, tel:Telefono, indirizzo:Indirizzo):
-        super().__init__(name, last, cf, tel, indirizzo)
